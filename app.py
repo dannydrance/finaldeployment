0a4a579e207d4d2f12a7e2b3daec1677ec8e7077
HAKIZAYEZU Daniel <90722447+dannydrance@users.noreply.github.com>
2024-10-02 11:53:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ if st.button('Predict'):
 
     # Display result
     if predicted_label[0][0] == 1:
-        st.success('Machine will fail soon.')
+        st.error('Machine will fail soon.')
     else:
         st.success('Machine is in good condition.')
-        st.success(predicted_label)
+        st.success(prediction[0][0])
